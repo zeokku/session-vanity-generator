1325ade5fdfc64b1830bdac650c4a2b0543d3137
Roman <r89534217610@gmail.com>
2022-10-21 23:12:32 +0400

    release 1.0

--- src/index.ts
+++ src/index.ts
@@ -3,82 +3,68 @@
 import libSodiumWrappers, { KeyPair } from "libsodium-wrappers-sumo";
 import { mn_decode, mn_encode } from "./mnemonic";
 
-async function getSodiumRenderer(): Promise<typeof libSodiumWrappers> {
-  await libSodiumWrappers.ready;
-  return libSodiumWrappers;
-}
+await libSodiumWrappers.ready;
 
-export type SessionKeyPair = {
-  /**
-   * The curve25519 pubkey with prepended 5
-   */
-  pubKey: ArrayBufferLike;
+const sodium = libSodiumWrappers;
 
-  /**
-   * The curve25519 secret key
-   */
-  privKey: ArrayBufferLike;
+console.clear();
 
-  ed25519KeyPair: KeyPair;
-};
-
-async function sessionGenerateKeyPair(seed: ArrayBuffer): Promise<SessionKeyPair> {
-  const sodium = await getSodiumRenderer();
-
-  const ed25519KeyPair = sodium.crypto_sign_seed_keypair(new Uint8Array(seed));
-  const x25519PublicKey = sodium.crypto_sign_ed25519_pk_to_curve25519(ed25519KeyPair.publicKey);
-  // prepend version byte (coming from `processKeys(raw_keys)`)
-  const origPub = new Uint8Array(x25519PublicKey);
-  const prependedX25519PublicKey = new Uint8Array(33);
-  prependedX25519PublicKey.set(origPub, 1);
-  prependedX25519PublicKey[0] = 5;
-  const x25519SecretKey = sodium.crypto_sign_ed25519_sk_to_curve25519(ed25519KeyPair.privateKey);
-
-  // prepend with 05 the public key
-  const x25519KeyPair = {
-    pubKey: prependedX25519PublicKey.buffer,
-    privKey: x25519SecretKey.buffer,
-    ed25519KeyPair,
-  };
-
-  return x25519KeyPair;
-}
-
-async function generateMnemonic(seedSize = 16) {
+/**
+ * @param size must be divisible by 8
+ */
+async function generateSeed(size = 16) {
   // Note: 4 bytes are converted into 3 seed words, so length 12 seed words
   // (13 - 1 checksum) are generated using 12 * 4 / 3 = 16 bytes.
   //   const seedSize = 32;
-  if (seedSize % 8) throw "Seed size must be divisible by 8";
+  if (size % 8) throw "Seed size must be divisible by 8";
+  const seed = sodium.randombytes_buf(size);
 
-  const seed = (await getSodiumRenderer()).randombytes_buf(seedSize);
-  const hex = Buffer.from(seed).toString("hex"); // toHex(seed);
-  return mn_encode(hex);
+  return seed;
 }
 
-const generateMnemonicAndKeyPair = async (mnemonic: string) => {
-  // if (generatedRecoveryPhrase === '') {
+function deriveMnemonic(seed: Uint8Array) {
+  return mn_encode(Buffer.from(seed).toString("hex"));
+}
 
-  let seedHex = mn_decode(mnemonic);
-  // handle shorter than 32 bytes seeds
-  const privKeyHexLength = 32 * 2;
-  if (seedHex.length !== privKeyHexLength) {
-    seedHex = seedHex.concat("0".repeat(32));
-    seedHex = seedHex.substring(0, privKeyHexLength);
+function deriveSeed(mnemonic: string) {
+  return new Uint8Array(Buffer.from(mn_decode(mnemonic), "hex"));
+}
+
+async function generateKeyPair(seed: Uint8Array) {
+  let extendedSeed = new Uint8Array(32);
+  extendedSeed.set(seed);
+
+  const ed25519KeyPair = sodium.crypto_sign_seed_keypair(new Uint8Array(extendedSeed));
+
+  const x25519PublicKey = sodium.crypto_sign_ed25519_pk_to_curve25519(ed25519KeyPair.publicKey);
+
+  const x25519SecretKey = sodium.crypto_sign_ed25519_sk_to_curve25519(ed25519KeyPair.privateKey);
+
+  // session address consists of a `05` prefix byte and a public key
+  return {
+    pub: x25519PublicKey,
+    sec: x25519SecretKey,
+  };
+}
+
+let prefixByte = Buffer.from("55", "hex").readUint8();
+let prefixTargetLength = 2;
+
+while (true) {
+  let seed = await generateSeed();
+  let { pub } = await generateKeyPair(seed);
+
+  let p = 0;
+  for (; p < prefixTargetLength; p += 1) {
+    if (pub[p] !== prefixByte) {
+      break;
+    }
   }
-  const seed = Buffer.from(seedHex, "hex"); //fromHex(seedHex);
-  const keyPair = await sessionGenerateKeyPair(seed);
-  const newHexPubKey = Buffer.from(keyPair.pubKey).toString("hex"); //StringUtils.decode(keyPair.pubKey, "hex");
 
-  //   setGeneratedRecoveryPhrase(mnemonic);
-  //   setHexGeneratedPubKey(newHexPubKey); // our 'frontend' sessionID
-  // }
+  if (p === prefixTargetLength) {
+    console.log("05" + Buffer.from(pub).toString("hex"));
+    console.log(deriveMnemonic(seed));
 
-  return newHexPubKey;
-};
-
-let mnemonic = await generateMnemonic();
-let pubAddress = await generateMnemonicAndKeyPair(mnemonic);
-
-console.clear();
-console.log(mnemonic, mnemonic.split(/\s/g).length);
-console.log(pubAddress);
+    break;
+  }
+}
